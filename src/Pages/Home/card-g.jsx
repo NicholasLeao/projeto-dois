b87e2abb18b6b2a1a9856c8db18ecc1937c7b31c
NicholasLeao <tropicaldata@gmail.com>
2022-04-27 09:54:14 -0300

    refactor big card

--- src/Pages/Home/card-g.jsx
+++ src/Pages/Home/card-g.jsx
@@ -1,43 +1,25 @@
 import styled from "styled-components";
-import imgsrc from "../../Assets/Images/3.png";
 
 function CardGrande(props) {
   const { data } = props;
   const imgUrl = data.corpo.match(/!\[.*?\]\((.*?)\)/)[1];
   return (
-    <S_div>
-      <S_infoDiv>
+    <StyledDiv>
+      <div className="info">
         <h2>{data.titulo}</h2>
         <hr></hr>
-      </S_infoDiv>
-      <S_gradient />
-      <S_imgDiv>
+      </div>
+      <div className="gradient" />
+      <div className="img-container">
         <img src={imgUrl} alt="placeholder" />
-      </S_imgDiv>
-    </S_div>
+      </div>
+    </StyledDiv>
   );
 }
 export default CardGrande;
 
-const S_gradient = styled.div`
-  width: inherit;
-  height: inherit;
-  position: absolute;
-  object-fit: cover;
-
-  /* COLOR */
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    0deg,
-    rgba(255, 255, 255, 1) 28%,
-    rgba(0, 0, 0, 0) 100%
-  );
-  opacity: 0.1;
-  /*  */
-`;
-
-const S_div = styled.div`
-  /* border: 1px solid #ebba3e; */
+// ==== STYLED COMPONENTS ====================
+const StyledDiv = styled.div`
   resize: none;
   width: 23rem;
   height: 12rem;
@@ -45,39 +27,59 @@ const S_div = styled.div`
   position: relative;
   overflow: hidden;
   box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 8px 0px;
-`;
 
-const S_imgDiv = styled.div`
-  width: inherit;
-  height: inherit;
-  object-fit: cover;
-  position: relative;
-  top: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  & img {
+  // ==== INFO ====================
+  & .info {
     position: absolute;
-    height: 100%;
-    opacity: 0.5;
+    z-index: 10;
+    margin-top: 15px;
+    margin: 15px;
+    & h2 {
+      font-size: 32px;
+      margin-right: 15px;
+      margin: 0;
+    }
+    & hr {
+      background-color: #ebba3e;
+      height: 3px;
+      border: none;
+    }
   }
-`;
 
-const S_infoDiv = styled.div`
-  position: absolute;
-  z-index: 10;
-  margin-top: 15px;
-  margin: 15px;
-  & h2 {
-    font-size: 32px;
-    margin-right: 15px;
-    margin: 0;
+  // ==== GRADIENT ====================
+  & .gradient {
+    width: inherit;
+    height: inherit;
+    position: absolute;
+    object-fit: cover;
+
+    /* COLOR */
+    background: rgb(255, 255, 255);
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 28%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    opacity: 0.2;
+    /*  */
   }
-  & hr {
-    background-color: #ebba3e;
-    height: 3px;
-    border: none;
+
+  // ==== IMAGE ====================
+  & .img-container {
+    width: inherit;
+    height: inherit;
+    object-fit: cover;
+    position: relative;
+    top: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & img {
+      position: absolute;
+      height: 100%;
+      opacity: 0.5;
+    }
   }
 `;
